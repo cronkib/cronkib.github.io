b28835245e606acf16ef9dca99ccb54159e1cf41
Travis Kern <kern.travis@gmail.com>
2021-05-18 00:30:27 -0400

    Work on site layout
    Pieces seem to be in place. Needs styling and some refinement.

---
+++ js/app.js
@@ -0,0 +1,194 @@
+var CronSite = (function() {
+    var render = function($, data) {
+        renderTitleSection($, data);
+        renderCoverPhoto($, data);
+        renderIntroduction($, data);
+        renderKnowledge($, data);
+        renderEducation($, data);
+        renderContact($, data);
+        renderTopBar($, data);
+    }
+
+    var renderTitleSection = function($, data) {
+        $("#header-brand-icon").attr("src", data["brandIcon"]);
+        $("#header-name").html(data["name"]);
+        $("#header-title").html(data["title"]);
+    }
+
+    var renderCoverPhoto = function($, data) {
+        $("#cover-photo").attr("src", data["coverPhoto"]);
+    }
+
+    var renderIntroduction = function($, data) {
+        var introElement = $("#introduction");
+        data["introduction"].forEach(function(paragraph) {
+            introElement.append("<p class='intro-paragraph'>" + paragraph + "</p>");
+        });
+    }
+
+    var renderKnowledge = function($, data) {
+        var knowledgeElement = $("#knowledge");
+        var knowledge = data["knowledge"];
+
+        var quote = knowledge["quote"];
+        if (quote) {
+            knowledgeElement.append("<p class='knowledge-quote'><em>\"" + quote["content"] + "\" - " + quote["credit"] + "</em></p>");
+        }
+
+        var intro = knowledge["introduction"];
+        if (intro) {
+            intro.forEach(function(paragraph) {
+                knowledgeElement.append("<p class='knowledge-intro-paragraph'>" + paragraph + "</p>");
+            });
+        }
+
+        var skills = knowledge["skills"];
+        if (skills) {
+            renderKnowledgeSkills(knowledgeElement, skills);
+        }
+    }
+
+    var renderKnowledgeSkills = function(container, skills) {
+        var accordionId = "accordionKnowledge";
+
+        var accordionHtml = "<div class='accordion' id='" + accordionId + "'>";
+
+        skills.forEach(function(skill, index) {
+            var skillTitle = skill["title"];
+            var skillContent = skill["content"];
+
+            var headingId = "heading" + skillTitle;
+            var collapseId = "collapse" + skillTitle;
+
+            var itemHtml = "<div class='accordion-item'>";
+
+            if (index == 0) {
+                itemHtml += "<h2 class='accordion-header' id='" + headingId + "'>";
+                itemHtml += "<button class='accordion-button' type='button' data-bs-toggle='collapse' data-bs-target='#" + collapseId + "' aria-expanded='true' aria-controls='" + collapseId + "'>";
+                itemHtml += skillTitle;
+                itemHtml += "</button>";
+                itemHtml += "</h2>";
+
+                itemHtml += "<div id='" + collapseId + "' class='accordion-collapse collapse show' aria-labelledby='" + headingId + "'>";
+                itemHtml += "<div class='accordion-body'>";
+                itemHtml += "<span>" + skillContent + "</span>";
+                itemHtml += "</div>";
+                itemHtml += "</div>";
+            }
+            else {
+                itemHtml += "<h2 class='accordion-header' id='" + headingId + "'>";
+                itemHtml += "<button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#" + collapseId + "' aria-expanded='false' aria-controls='" + collapseId + "'>";
+                itemHtml += skillTitle;
+                itemHtml += "</button>";
+                itemHtml += "</h2>";
+
+                itemHtml += "<div id='" + collapseId + "' class='accordion-collapse collapse' aria-labelledby='" + headingId + "'>";
+                itemHtml += "<div class='accordion-body'>";
+                itemHtml += "<span>" + skillContent + "</span>";
+                itemHtml += "</div>";
+                itemHtml += "</div>";
+            }
+
+            itemHtml += "</div>";
+            accordionHtml += itemHtml;
+        });
+
+        accordionHtml += "</html>";
+        container.append(accordionHtml);
+    }
+
+    var renderEducation = function($, data) {
+        var education = data["education"];
+        $("#education").append("<p class='education-paragraph'>" + education + "</p>");
+    }
+
+    var renderContact = function($, data) {
+        var contactElement = $("#contact");
+        var contactHtml = "<div>";
+
+        var contactList = data["contact"];
+        contactList.forEach(function(info) {
+            var type = info["type"];
+            var link = info["link"];
+            var icon = info["icon"];
+
+            var infoHtml = "<div>";
+
+            if (type === "Email") {
+                infoHtml += "<a href='mailto:" + link + "' target='_top'>";
+
+                if (icon) {
+                    infoHtml += "<img src='" + icon + "' width='28', height='28' />";
+                }
+
+                infoHtml += "<span class='contact-link-name'>" + link + "</span>";
+                infoHtml += "</a>"
+
+            }
+            else {
+                infoHtml += "<a href='" + link + "' target='_blank'>";
+
+                if (icon) {
+                    infoHtml += "<img src='" + icon + "' width='28', height='28' />";
+                }
+
+                infoHtml += "<span class='contact-link-name'>" + type + "</span>";
+                infoHtml += "</a>"
+            }
+
+            infoHtml += "</div>";
+            contactHtml += infoHtml;
+        });
+
+        contactHtml += "</div>";
+        contactElement.append(contactHtml);
+    }
+
+    var renderTopBar = function($, data) {
+        var contactElement = $("#topbar");
+        var contactHtml = "";
+
+        var contactList = data["contact"];
+        contactList.forEach(function(info) {
+            var type = info["type"];
+            var link = info["link"];
+            var icon = info["icon"];
+
+            var infoHtml = "<div class='top-contact-link'>";
+
+            if (type === "Email") {
+                infoHtml += "<a href='mailto:" + link + "' target='_top'>";
+
+                if (icon) {
+                    infoHtml += "<img src='" + icon + "' width='28', height='28' />";
+                }
+
+                infoHtml += "</a>"
+
+            }
+            else {
+                infoHtml += "<a href='" + link + "' target='_blank'>";
+
+                if (icon) {
+                    infoHtml += "<img src='" + icon + "' width='28', height='28' />";
+                }
+
+                infoHtml += "</a>"
+            }
+
+            infoHtml += "</div>";
+            contactHtml += infoHtml;
+        });
+
+        var resume = data["resume"];
+        var resumeLocation = resume["location"];
+        var resumeIcon = resume["icon"];
+
+        contactHtml += "<div class='top-resume'><strong>Resume</strong> <img src='" + resumeIcon + "' width='28' /></div>"
+        contactElement.append(contactHtml);
+    }
+
+    return {
+        render: render 
+    }
+})()
---
+++ js/data.js
@@ -0,0 +1,65 @@
+var CronData = {
+    "name": "Travis Kern",
+    "brandIcon": "images/CronoIcon.png",
+    "title": "Sr. Software Engineer | Leader | Innovator",
+    "introduction": [
+        "Creative and passionate full-stack programmer with years of experience in web application development. I'm highly adept at learning new languages, picking up new technologies, and overall acquiring new skills. My strengths lie in systems design and implementation, writing clean, maintainable code, and being a team player. I have a strong knack for appreciating the details of systems and utilizing that to write software that is functional, correct and extendable",
+        "I also stay active, love traveling, play video games, and have two big dogs."
+    ],
+    "coverPhoto": "images/western-pa-cropped.jpeg",
+    "knowledge": {
+        "quote": {
+            "content": "Condensing fact from the vapor of nuance.",
+            "credit": "Neuromancer"
+        },
+        "introduction": [
+            "For a decade, I have been building systems from the ground up, backend to frontend, as well as enhancing existing products to solve problems. Whether I am designing a complex database schema, writing perfomant SQL, implementing RESTful web services, or developing a modern frontend, I accomplish my goals. By gathering information and applying a results-oriented mindset, there is very little that stands betweeen me and job well done.",
+            "My knowledge includes, but is certainly not limited to, the following:"
+        ],
+        "skills": [
+            {
+                "title": "Languages",
+                "content": "<strong>Java</strong>, <strong>SQL</strong>, <strong>Ruby</strong>, GraphQL, TypeScript, Python, JavaScript, Swift, HTML, XML, JSON, CSS, Objective-C"
+            },
+            {
+                "title": "Frameworks",
+                "content": "<strong>Spring</strong>, <strong>Node.js</strong>, <strong>Graphlient</strong>, <strong>psycopg2</strong>, Angular, Hibernate, d3.js, Express.js, jQuery, React, Google Maps API, Freemarker, Bootstrap, iText, SmartXLS, JUnit, SmartGWT, GWT"
+            },
+            {
+                "title": "Tools",
+                "content": "<strong>Maven</strong>, <strong>Git</strong>, <strong>Bundler</strong>, <strong>PostgreSQL</strong>, MySQL, Eclipse, IntelliJ, Jira, Jenkins, Salt, NPM, VIM, SQLite, Tomcat, Nginx, XCode, Crystal Reports"
+            },
+            {
+                "title": "Skills",
+                "content": "<strong>Leadership</strong>, <strong>Software Analysis & Design</strong>, <strong>Database Schema Design</strong>, Mentoring, Interviewing, Web Application Development, MVC, Object-Oriented Programming, Communication, Accountability, Adaptability, Confidence"
+            }
+        ]
+    },
+    "education": "<strong>Misericordia University</strong><br />B.A. Computer Science<br />Summa Cum Laude<br /><i>2008 - 2012</i>",
+    "contact": [
+        {
+            "type": "LinkedIn",
+            "link": "https://www.linkedin.com/in/travisjkern",
+            "icon": "images/linkedin.png"
+        },
+        {
+            "type": "Github",
+            "link": "https://github.com/cronkib",
+            "icon": "images/github.png"
+        },
+        {
+            "type": "Twitter",
+            "link": "https://twitter.com/cronkib",
+            "icon": "images/twitter.png"
+        },
+        {
+            "type": "Email",
+            "link": "kern.travis@gmail.com",
+            "icon": "images/email.png"
+        },
+    ],
+    "resume": {
+        "location": "files/TravisKern_Resume.pdf",
+        "icon": "images/resume.png"
+    }
+}
